f560e5aa4482b93b0e517ee99843018131a11cc0
kruczjak <kruczjak@gmail.com>
2016-03-20 21:38:09 +0100

    Better deploy script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 touch ./deployed
+(cd ~/iiet-chat && git pull)
+(cd ~/iiet-chat && meteor add rocketchat:hubot && meteor build --server https://chat.dev.i$
+(cd ~/server/bundle/programs/server && npm install)
+
+export ROOT_URL=https://chat.dev.iiet.pl/
+export MONGO_URL=mongodb://localhost:27017/rocketchat
+export PORT=9901
+pm2 restart main
